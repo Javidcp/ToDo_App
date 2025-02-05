f1fedbb2e4bb57d31dcd6860f4b3e8ced25705c4
javid ibrahim <ibrupottayil@gmail.com>
2025-02-05 12:35:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Todo from "./components/Todo";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="main-area">
+      <Todo />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+import "./Todo.css";
+import { Button, Navbar, Container } from 'react-bootstrap';
+
+
+const Todo = () => {
+
+    const [todoList, setTodoList] = useState([]);
+
+    let saveToDoList = (event) => {
+        event.preventDefault();
+
+        let toname = event.target.toname.value;
+        if (!todoList.includes(toname)) {
+            let finalDoList = [...todoList, toname.toUpperCase()];
+            setTodoList(finalDoList)
+        } else {
+            alert("ToDo Name is Already Exists...")
+        }
+        
+    }
+
+    let list = todoList.map((value, index) => {
+
+        return (
+            <TodoListItems value={value} key={index} indexNumber={index} todoList={todoList} setTodoList={setTodoList} />
+        )
+    })
+    return (
+    <div className="main-form">
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand>ToDo List</Navbar.Brand>
+            </Container>
+        </Navbar>
+        <br /><br />
+        <form className="TodoForm" onSubmit={saveToDoList}>
+            <input 
+                type="text" 
+                name="toname"
+                className="todo-input" 
+                placeholder="What is the task today?"
+            />
+            <button type="submit" className="todo-btn">Add</button>
+        </form>
+
+        <div className="outerDiv">
+            <ul>
+                {list}
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Todo;
+
+
+const TodoListItems = ({value, indexNumber, todoList, setTodoList}) => {
+
+
+    let deleteRow = () => {
+        let finalData = todoList.filter((v, i) => i != indexNumber)
+        setTodoList(finalData);
+    }
+
+    
+
+    return (
+        <li>{indexNumber + 1} . {value}<span onClick={deleteRow}><Button variant="danger">Danger</Button></span></li>
+    )
+}
